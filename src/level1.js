fbe579ac567c8569423bbd6fd245600fd78a63d7
gonzzoh <gmeza0118@gmail.com>
2023-08-07 10:57:05 -0400

    Changed Names

--- src/level1.js
+++ src/level1.js
@@ -1,25 +1,55 @@
-
+const browserWidth = window.innerWidth;
+const browserHeight = window.innerHeight;
 const k = kaboom({
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: browserWidth,
+  height: browserHeight,
 });
 
 k.loadSprite("slime", "sprites/slime/slime-idle-1.png")
 k.loadSprite("background0", "sprites/stringstar fields/background_0.png");
 k.loadSprite("background1", "sprites/stringstar fields/background_1.png");
 k.loadSprite("background2", "sprites/stringstar fields/background_2.png");
+// k.loadSpriteAtlas('sprites/tiles', {
+    
+// })
 
 add([
     sprite('background0'),
     fixed(),
-    scale(5.5)
+    scale(4)
 ]);
 
 add([
     sprite('background0'),
     fixed(),
     pos(1000, 0),
-    scale(5.5)
+    scale(4)
+]).flipX = true
+
+add([
+    sprite('background1'),
+    fixed(),
+    scale(4)
+]);
+
+add([
+    sprite('background1'),
+    fixed(),
+    pos(1000, 0),
+    scale(4)
+]).flipX = true
+
+add([
+    sprite('background2'),
+    fixed(),
+    scale(4)
+]);
+
+add([
+    sprite('background2'),
+    fixed(),
+    pos(1000, 0),
+    scale(4)
 ]).flipX = true
 
 k.add([
@@ -29,38 +59,38 @@ k.add([
 
 k.onClick(() => k.addKaboom(k.mousePos()));
 
-const c = kaboom({
-    width: window.innerWidth,
-    height: window.innerHeight
-});
+// const c = kaboom({
+//     width: window.innerWidth,
+//     height: window.innerHeight
+// });
 
-c.loadSprite("slime", "sprites/slime/slime-idle-1.png")
-c.loadSprite("background0", "sprites/stringstar fields/background_0.png")
-c.loadSprite("background1", "sprites/stringstar fields/background_1.png")
-c.loadSprite("background2", "sprites/stringstar fields/background_2.png")
+// c.loadSprite("slime", "sprites/slime/slime-idle-1.png")
+// c.loadSprite("background0", "sprites/stringstar fields/background_0.png")
+// c.loadSprite("background1", "sprites/stringstar fields/background_1.png")
+// c.loadSprite("background2", "sprites/stringstar fields/background_2.png")
 
-add([
-    sprite('background0'),
-    fixed(),
-    scale(5.5)
-]);
+// add([
+//     sprite('background0'),
+//     fixed(),
+//     scale(5.5)
+// ]);
 
-add([
-    sprite('background0'),
-    fixed(),
-    pos(1000, 0),
-    scale(5.5)
-]).flipX = true
+// add([
+//     sprite('background0'),
+//     fixed(),
+//     pos(1000, 0),
+//     scale(5.5)
+// ]).flipX = true
 
 
-c.add([
-    c.pos(120, 80),
-    c.sprite("slime"),
-])
+// c.add([
+//     c.pos(120, 80),
+//     c.sprite("slime"),
+// ])
 
-c.onClick(() => c.addKaboom(c.mousePos()));
+// c.onClick(() => c.addKaboom(c.mousePos()));
 
-kaboom();
+// kaboom();
 
 
 
