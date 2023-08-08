8e0310c2ed1fa36b8a6a317dccdeecb80b49938e
kaylacampbell1 <kaylacampbell3003@gmail.com>
2023-08-08 00:06:55 -0400

    sprite reacts tobuttons and platforms move a regenerate

--- src/level1.js
+++ src/level1.js
@@ -1,17 +1,16 @@
 const browserWidth = window.innerWidth;
 const browserHeight = window.innerHeight;
 let k = kaboom({
-    width: 630,
-    height: browserHeight,
-    canvas: document.querySelector("canvas"),
-    backgroundAudio: true
-})
-
+  width: 630,
+  height: 770,
+  canvas: document.querySelector("canvas"),
+  backgroundAudio: false,
+});
 
 //! --------------------------------- SPRITES -------------------------------- */
-loadSprite("platform", "sprites/tiles/platform.png")
-loadSprite("slime", "sprites/slime/slime-idle-1.png")
-loadSprite("castleWall", "sprites/stringstarfields/castlebg.png")
+loadSprite("platform", "sprites/tiles/platform.png");
+loadSprite("slime", "sprites/slime/slime-idle-1.png");
+loadSprite("castleWall", "sprites/stringstarfields/castlebg.png");
 //  {
 //     sliceX: 1,
 //     sliceY: 1,
@@ -24,116 +23,134 @@ loadSprite("castleWall", "sprites/stringstarfields/castlebg.png")
 //     anims: {'move-anim': {from: 0, to: 7, loop: true }}
 // });
 
-setGravity(100);
-scene("opening", () => {
-    
-})
+scene("opening", () => {});
 scene("game", () => {
-    loadSpriteAtlas('sprites/tiles/Tileset.png', {
-        "floor": { x: 15, y: 1, width: 50, height: 75 },
-        "back": { x: 65, y: 35, width: 50, height: 75 },
-        "wallL": { x: 0, y: 25, width: 25, height: 75 },
-        "wallC": { x: 55, y: 25, width: 25, height: 75 },
-        "wallR": { x: 99, y: 25, width: 35, height: 75 },
-    })
+  loadSpriteAtlas("sprites/tiles/Tileset.png", {
+    floor: { x: 15, y: 1, width: 50, height: 75 },
+    back: { x: 65, y: 35, width: 50, height: 75 },
+    wallL: { x: 0, y: 25, width: 25, height: 75 },
+    wallC: { x: 55, y: 25, width: 25, height: 75 },
+    wallR: { x: 99, y: 25, width: 35, height: 75 },
+  });
 
-    //! ----------------------------------- MAP ---------------------------------- */
+  //! -------------------------------- PLATFORMS ------------------------------- */
+  let platformY = 3;
+  let gameSpeed = 20;
+  function spawnPlatform() {
+    let platformX = rand(90, 360);
     add([
-        sprite('castleWall'),
-        fixed(),
-        pos(1.7, 0),
-        scale(1, 1.35)])
+      sprite("platform"),
+      fixed(),
+      area(),
+      pos(platformX, platformY),
+      move(DOWN, gameSpeed),
+      scale(0.24, 0.15),
+      offscreen({ destroy: true }),
+    ]);
+  }
 
-    const map = addLevel([
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "234                   234",
-        "0000000000000000000000000",
-        "0000000000000000000000000",
-        "0000000000000000000000000",
-    ], {
-        tileWidth: 25,
-        tileHeight: 25,
-        tiles: {
-            0: () => [sprite('floor'), area(), body({ isStatic: true })],
-            1: () => [sprite('back')],
-            2: () => [sprite('wallL'), area(), body({ isStatic: true })],
-            3: () => [sprite('wallC'), area(), body({ isStatic: true })],
-            4: () => [sprite('wallR'), area(), body({ isStatic: true })]
-        }
-    })
+  loop(6, () => {
+    spawnPlatform();
+  });
 
-    // const platGap = 100;
-    // const offset = rand(-50, 50)
+  //! ----------------------------------- MAP ---------------------------------- */
+  add([sprite("castleWall"), fixed(), pos(1.7, 0), scale(1, 1.35)]);
 
-    //! -------------------------------- CHARACTER ------------------------------- */
-    let player = add([
-        pos(120, 80),
-        scale(2.5),
-        sprite("slime"),
-        area(),
-        body(),
-        anchor('center'),
-    ]).flipX = true;
+  const map = addLevel(
+    [
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "24                     24",
+      "0000000000000000000000000",
+      "0000000000000000000000000",
+      "0000000000000000000000000",
+    ],
+    {
+      tileWidth: 25,
+      tileHeight: 25,
+      tiles: {
+        0: () => [sprite("floor"), area(), fixed(), body({ isStatic: true })],
+        1: () => [sprite("back")],
+        2: () => [sprite("wallL"), area(), fixed(), body({ isStatic: true })],
+        3: () => [sprite("wallC"), area(), fixed(), body({ isStatic: true })],
+        4: () => [sprite("wallR"), area(), fixed(), body({ isStatic: true })],
+      },
+    }
+  );
 
-    //! -------------------------------- PLATFORMS ------------------------------- */
-    let platformX = rand(100, )
-    add([
-        sprite('platform'),
-        fixed(),
-        area(),
-        pos(110, 400),
-        scale(0.24, 0.15),
-    ])
+  // const platGap = 100;
+  // const offset = rand(-50, 50)
 
-    //! -------------------------------- CONTROLS -------------------------------- */
+  //! -------------------------------- CHARACTER ------------------------------- */
+  const player = add([
+    sprite("slime"),
+    pos(265, 662),
+    scale(2.5),
+    area(),
+    body(),
+  ]);
+  player.flipX = true;
+
+  //! -------------------------------- CONTROLS -------------------------------- */
+  onKeyPress("space", () => {
+    setGravity(500);
+    // if (player.grounded()) {
+    player.jump(300);
+    // }
+  });
+  onKeyDown("left", () => {
+    player.move(-200, 0);
+    player.flipX = false;
+  });
+  onKeyDown("right", () => {
+    player.move(200, 0);
+    player.flipX = true;
+  });
+  onCollide("slime", "platform", (obj) => {
+    obj.body = { isStatic: true };
+  });
+  onCollide("slime", "wallL", () => {
     onKeyPress("space", () => {
-        player.jump()
-    })
+      player.jump(400);
+      player.flipX = false;
+    });
+  });
+  onCollide("slime", "wallR", () => {
+    onKeyPress("space", () => {
+      player.jump(400);
+      player.flipX = true;
+    });
+  });
 
-    onClick(() => k.addKaboom(k.mousePos()));
-})
+  // onClick(() => k.addKaboom(k.mousePos()));
+});
 
-go("game")
+scene("gameover", () => {});
 
-scene("gameover", () => {
-    
-})
+go("game");
