ee30e57fb25762f1571e20d9ed899e9c379f2d61
SirreaReeves <reevessirrea@gmail.com>
2023-08-09 10:35:11 -0400

    Updated screens

--- src/level1.js
+++ src/level1.js
@@ -11,78 +11,106 @@ let k = kaboom({
 loadSprite("platform", "sprites/tiles/platform.png");
 loadSprite("slime", "sprites/slime/slime-idle-1.png");
 loadSprite("castleWall", "sprites/stringstarfields/castlebg.png");
-//  {
-//     sliceX: 1,
-//     sliceY: 1,
-//     anims: {'idle-anim': {from: 0, to: 7, loop: true }}
-// });
-
-// loadSprite("move-slime", 'sprites/slime/slime-move-0.png', {
-//     sliceX: 1,
-//     sliceY: 1,
-//     anims: {'move-anim': {from: 0, to: 7, loop: true }}
-// });
-
+loadSprite('background-0', 'sprites/stringstarfields/background_0.png');
+loadSprite('background-1', 'sprites/stringstarfields/background_1.png');
+loadSprite('background-2', 'sprites/stringstarfields/background_2.png');
 //! -------------------------------- START SCREEN -------------------------------- */
 scene("start", () => {
-      // loadFont();
-      const bgColor = color(78,138,198);
+  loadFont("custom-font", "fonts/Extrude-90aK.ttf");
   
-      add([
-          rect(width(), height()), 
-          bgColor,
-          pos(width() / 2, height() / 2),
-          anchor("center"),
-      ]);
-  
-      const startGame = add([
-          text("Press Space to Start", {
-            transform: (idx, ch) => ({
-              color: color(78,138,198),
-              // pos: vec2(0, wave(-4, 4, time() * 4 + idx * 0.5)),
-              // scale: wave(1, 1.2, time() * 3 + idx),
-              // angle: wave(-24, 9, time() * 3 + idx),
-            }),
-          }),
-          pos(width() / 2, height() / 1.5),
-          scale(0.75, 0.75),
-          anchor("center"),
-          area(),
-        ]);
-      
-      const titleText = add([
-          text("Send the Slime", {
-            transform: (idx, ch) => ({
-              color: color(78,138,198),
-              pos: vec2(0, shake(-4, 4, time() * 4 + idx * 0.5)),
-              scale: wave(1, 1.2, time() * 3 + idx),
-              angle: wave(-24, 9, time() * 3 + idx),
-            }),
-          }),
-          pos(width()/2,startGame.pos.y/2),
-          scale(1.5),
-          anchor("center"),
-          area(),
-        ])
-        onKeyPress("space", () => {
-        go("game");   
-      });
-    });
-    
-    // scene("opening", () => { });
-    scene("game", () => {
-      loadSpriteAtlas("sprites/tiles/Tileset.png", {
-        floor: { x: 15, y: 1, width: 50, height: 75 },
-        back: { x: 65, y: 35, width: 50, height: 75 },
+  add([
+    sprite('background-0'),
+    fixed(), 
+    scale(5),
+])
+
+add([
+    sprite('background-0'),
+    fixed(),
+    pos(1000, 0),
+    scale(5),
+]).flipX = true
+
+add([
+  sprite('background-1'),
+  fixed(),
+  scale(4.35)
+])
+
+add([
+  sprite('background-1'),
+  fixed(),
+  pos(1000, 0),
+  scale(4.35),
+]).flipX = true
+
+add([
+  sprite('background-2'),
+  fixed(),
+  scale(4.35)
+])
+
+add([
+  sprite('background-2'),
+  fixed(),
+  pos(1000, 0),
+  scale(4.35),
+]).flipX = true
+
+  const startGame = add([
+    text("Press Space to Start", {
+      font: "custom-font",
+      size: 30,
+    }),
+    pos(width() / 2, height() / 1.5),
+    scale(0.75, 0.75),
+    anchor("center"),
+    area(),
+  ]);
+
+  const tutorial = add([
+    text("How to Play", {
+      font: "custom-font",
+      size: 30,
+    }),
+    pos(width() / 2, height() / 2),
+    scale(1),
+    anchor("center"),
+    area(),
+  ]);
+
+  const titleText = add([
+    text("Send the Slime", {
+      font: "custom-font",
+      size: 45,
+      transform: (idx) => ({
+        angle: (-24, 9, time() * 10 + idx),
+      }),
+    }),
+    pos(width() / 2, startGame.pos.y / 3),
+    scale(1.5),
+    anchor("center"),
+    area(),
+  ])
+  onKeyPress("space", () => {
+    go("game");
+  });
+});
+
+//! --------------------------------- TILESET -------------------------------- */
+scene("game", () => {
+  loadSpriteAtlas("sprites/tiles/Tileset.png", {
+    floor: { x: 15, y: 1, width: 50, height: 75 },
+    back: { x: 65, y: 35, width: 50, height: 75 },
     wallL: { x: 0, y: 25, width: 25, height: 75 },
     wallC: { x: 55, y: 25, width: 25, height: 75 },
     wallR: { x: 99, y: 25, width: 35, height: 75 },
   });
-  
+
   //! -------------------------------- PLATFORMS ------------------------------- */
   let platformY = 620;
   let gameSpeed = 100;
-  
+
   function spawnPlatform() {
     let platformX = rand(90, 360);
     let platformHeight = rand(0.17, 0.25)
@@ -98,15 +126,15 @@ scene("start", () => {
     platformY -= 100
   }
   // for (let i = 1; i < 8; i++) {
-    //   spawnPlatform()
-    // }
-    loop(0.3, () => {
-      spawnPlatform();
-    });
-    
-    //! ----------------------------------- MAP ---------------------------------- */
-    add([sprite("castleWall"), fixed(), pos(1.7, 0), scale(1, 1.35)]);
-    
+  //   spawnPlatform()
+  // }
+  loop(0.3, () => {
+    spawnPlatform();
+  });
+
+  //! ----------------------------------- MAP ---------------------------------- */
+  add([sprite("castleWall"), fixed(), pos(1.7, 0), scale(1, 1.35)]);
+
   const map = addLevel(
     [
       "24                     24",
@@ -154,38 +182,38 @@ scene("start", () => {
         4: () => [sprite("wallR"), area(), fixed(), body({ isStatic: true })],
       },
     }
-    );
-    
-    // const platGap = 100;
-    // const offset = rand(-50, 50)
-    
-    //! -------------------------------- CHARACTER ------------------------------- */
-    const player = add([
-      sprite("slime"),
-      pos(265, 680),
-      scale(2.5),
-      area(),
-      body(),
-    ]);
-    player.flipX = true;
+  );
 
-    // player.onUpdate(() => {
-      //   campPos(player.pos.x, 0)
-      // })
-      
-      //! -------------------------------- CONTROLS -------------------------------- */
-      let wallTouch = 0;
-      let traveled = 150;
-      // if (!player.isGrounded()) traveled = 0
-      
-      onKeyPress("space", () => {
-        setGravity(500);
+  // const platGap = 100;
+  // const offset = rand(-50, 50)
+
+  //! -------------------------------- CHARACTER ------------------------------- */
+  const player = add([
+    sprite("slime"),
+    pos(265, 680),
+    scale(2.5),
+    area(),
+    body(),
+  ]);
+  player.flipX = true;
+
+  // player.onUpdate(() => {
+  //   campPos(player.pos.x, 0)
+  // })
+
+  //! -------------------------------- CONTROLS -------------------------------- */
+  let wallTouch = 0;
+  let traveled = 150;
+  // if (!player.isGrounded()) traveled = 0
+
+  onKeyPress("space", () => {
+    setGravity(500);
     if (player.isGrounded() && traveled >= 0) {
       player.jump(300 + Number(traveled));
-        traveled = 0
-    } 
+      traveled = 0
+    }
   });
-  
+
   onKeyPress("up", () => {
     setGravity(500);
     if (player.isGrounded() && traveled >= 0) {
@@ -193,30 +221,30 @@ scene("start", () => {
       traveled = 0
     }
   });
-  
+
   onKeyDown("left", () => {
     // loop(1, () => {
-      //   if (player.isGrounded()) { 
-        //     traveled++
-        //   } 
-        //   if (traveled > 5) traveled = 5
-        // })
-        player.move(-200, 0);
-        player.flipX = false;
-      });
-      
-      onKeyDown("right", () => {
-        // loop(1, () => {
-          //   if (player.isGrounded()) { 
-            //     traveled++
-            //   } 
-            //   if (traveled > 5) traveled = 5
-            // })
-            player.move(200, 0);
-            player.flipX = true;
-          });
-          
-          // player.onCollideEnd("platform", (player) => {
+    //   if (player.isGrounded()) { 
+    //     traveled++
+    //   } 
+    //   if (traveled > 5) traveled = 5
+    // })
+    player.move(-200, 0);
+    player.flipX = false;
+  });
+
+  onKeyDown("right", () => {
+    // loop(1, () => {
+    //   if (player.isGrounded()) { 
+    //     traveled++
+    //   } 
+    //   if (traveled > 5) traveled = 5
+    // })
+    player.move(200, 0);
+    player.flipX = true;
+  });
+
+  // player.onCollideEnd("platform", (player) => {
   //   player.body({ isStatic: true });
 
   // });
@@ -224,21 +252,117 @@ scene("start", () => {
   player.onCollide("wallL", () => {
     traveled = 0
   });
-  
+
   player.onCollide("wallR", () => {
     traveled = 0
   })
-  
+
   player.onCollide('floor', () => {
     traveled = 0
   })
-  
+
   // onClick(() => k.addKaboom(k.mousePos()));
 });
 
 
- //! -------------------------------- GAMEOVER SCREEN -------------------------------- */
-scene("gameover", () => { });
-      
+//! -------------------------------- GAMEOVER SCREEN -------------------------------- */
+// scene("game over", () => {
+//   loadFont("custom-font", "fonts/Extrude-90aK.ttf");
+
+//   add([
+//     sprite('background-0'),
+//     fixed(), 
+//     scale(5),
+// ])
+
+// add([
+//     sprite('background-0'),
+//     fixed(),
+//     pos(1000, 0),
+//     scale(5),
+// ]).flipX = true
+
+// add([
+//   sprite('background-1'),
+//   fixed(),
+//   scale(4.35)
+// ])
+
+// add([
+//   sprite('background-1'),
+//   fixed(),
+//   pos(1000, 0),
+//   scale(4.35),
+// ]).flipX = true
+
+// add([
+//   sprite('background-2'),
+//   fixed(),
+//   scale(4.35)
+// ])
+
+// add([
+//   sprite('background-2'),
+//   fixed(),
+//   pos(1000, 0),
+//   scale(4.35),
+// ]).flipX = true
+
+//   const retry = add([
+//     text("Press Enter to Try Again", {
+//       font: "custom-font",
+//       size: 30,
+//       transform: (idx) => ({
+//         // color: rgb(255, 255, 255),
+//         pos: vec2(0, wave(-4, 4, time() * 4 + idx * 0.5)),
+//         scale: wave(1, 1.2, time() * 3 + idx),
+//         angle: wave(-24, 9, time() * 3 + idx),
+//       }),
+//     }),
+//     pos(width() / 2, height() / 1.5),
+//     scale(0.75, 0.75),
+//     anchor("center"),
+//     area(),
+//   ]);
+
+//   const titleText = add([
+//     text("Game Over", {
+//       font: "custom-font",
+//       size: 45,
+//       transform: (idx) => ({
+//         // color: rgb(255, 255, 255),
+//         pos: vec2(0, wave(-4, 4, time() * 4 + idx * 0.5)),
+//         scale: wave(1, 1.2, time() * 3 + idx),
+//         angle: wave(-24, 9, time() * 3 + idx),
+//       }),
+//     }),
+//     pos(width() / 2, retry.pos.y / 2),
+//     scale(1.5),
+//     anchor("center"),
+//     area(),
+//   ])
+  
+//   // const displayScore = add([
+//   //   text(`Score: ${score.value}`, {
+//   //     transform: (idx) => ({
+//   //       // color: rgb(255, 255, 255),
+//   //       pos: vec2(0, wave(-4, 4, time() * 4 + idx * 0.5)),
+//   //       scale: wave(1, 1.2, time() * 3 + idx),
+//   //       angle: wave(-24, 9, time() * 3 + idx),
+//   //     }),
+//   //   }),
+//   //   pos(width() / 2, retry.pos.y / 1.5),
+//   //   scale(1.5),
+//   //   anchor("center"),
+//   //   area(),
+//   // ])
+  
+//   onKeyPress("enter", () => {
+//     go("game");
+//   });
+// });
+
+//! -------------------------------- INVOKATION -------------------------------- */
 go("game");
 go('start');
+// go('game over');
